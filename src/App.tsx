c683aa02434d9a5280fdef43c947e996487f2c52
tsuda-hiroshi <tsuda-hiroshi@systemsoft.co.jp>
2021-12-13 11:51:23 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import './App.css';
 import Countdown from 'react-countdown'
 
 export const App = () => {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <h1>Please wait a moment now</h1>
