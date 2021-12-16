4e35b85a9c75fa9d26f665c56dafd5a8cd37a8ab
tsuda-hiroshi <tsuda-hiroshi@systemsoft.co.jp>
2021-12-16 14:26:19 +0900

    s

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import './App.css';
 import Countdown from 'react-countdown'
 
 export const App = () => {
+  
   return (
     <div className="App">
+      <div className="container"><p>www</p></div>
       <h1>Please wait a moment now</h1>
       <p className="wf-kokoro">少々お待ちください</p>
       <Countdown date={Date.now() + 300000} />
